ee76006a7faf8c585bab96998430d30ad4ef40f2
Rajakumardev <rajakumarofficial@gmail.com>
2023-05-29 11:01:45 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: "#483A58"
+        },
+        accent: {
+          900: "#241d2c"
+        }
+      }
+    },
   },
   plugins: [],
 }
