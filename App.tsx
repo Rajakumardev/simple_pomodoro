a770e468e0d7aab0f3d1649817ef094db3182cb2
Rajakumardev <rajakumarofficial@gmail.com>
2023-05-29 10:26:01 +0530

    setup

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+
+const App = (): JSX.Element => {
+ 
+
+  return (
+    <View></View>
+  );
+}
+
+
+export default App;
