f9b5b680e222b99374b5a4634b0370c9e1138336
Rajakumardev <rajakumarofficial@gmail.com>
2023-06-14 21:13:31 +0530

    refactor Timer into new component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,53 +4,40 @@ import { Text, View } from 'react-native';
 import { Svg, Circle } from 'react-native-svg';
 import { ControlButton } from '../Component';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useTimerStore } from '../store';
+import { PASUED, PLAYING } from '../constants';
+import { Timer } from '../Component/Timer/Timer';
 
 const StyledView = styled(View);
-const StyledText = styled(Text);
-const CIRCLE_LENGTH = 1000;
-const radius = CIRCLE_LENGTH / (2 * Math.PI);
-const viewBoxHeight = 400;
-const viewBoxWidth = 400;
 
 export const Home = (): JSX.Element => {
+	const { timerState, setTimerState } = useTimerStore();
+
+	const onControlBtnPress = (newTimerState = timerState) => {
+		setTimerState(newTimerState);
+	};
+
 	return (
 		<StyledView className="bg-primary-900 flex-1">
-			<StyledView className="relative flex-1">
-				<Svg
-					className="fill-primary-900"
-					viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
-				>
-					<Circle
-						cx={viewBoxWidth / 2}
-						cy={viewBoxHeight / 2}
-						r={radius}
-						stroke={'#fff'}
-						strokeWidth={20}
-						strokeDasharray={CIRCLE_LENGTH}
-						strokeDashoffset={CIRCLE_LENGTH * 0}
-					/>
-				</Svg>
-				<StyledView className="flex absolute justify-center items-center w-full h-full">
-					<StyledText className={`text-white  z-10 text-3xl`}>
-						25:00
-					</StyledText>
-				</StyledView>
-			</StyledView>
+			<Timer />
 			<StyledView className="flex-1 px-4 flex">
 				<StyledView className="flex flex-row items-start justify-between">
 					<ControlButton
+						onPressCallback={() => onControlBtnPress(PLAYING)}
 						className="text-xl w-20 h-20 flex justify-center 
 							items-center border-2 border-white"
 					>
 						<Icon name="play" size={20} color={'#fff'} />
 					</ControlButton>
 					<ControlButton
+						onPressCallback={() => onControlBtnPress(PASUED)}
 						className="text-xl w-20 h-20 flex justify-center 
 							items-center border-2 border-white"
 					>
 						<Icon name="pause" size={20} color={'#fff'} />
 					</ControlButton>
 					<ControlButton
+						onPressCallback={() => onControlBtnPress(PASUED)}
 						className="text-xl w-20 h-20 flex justify-center 
 							items-center border-2 border-white"
 					>
